71736a1d23e41f8f498e274c1ffed4dbf67d9048
Malcolm Harrow <harrow.malcolm@gmail.com>
2024-12-03 16:01:33 -0500

    Move to yosys

--- AddressDecoder.v
+++ AddressDecoder.v
@@ -23,20 +23,23 @@ module AddressDecoder(
 	output o_EXPSEL_n
 	);
 	
-	wire ram = (~i_CPUSP_n && ~i_AS_n && (i_A[23:20] == 4'h0));
-	wire isram = (ram && i_BOOT) || (ram && ~i_A[19] && ~i_BOOT) || (ram && ~i_A[18] && ~i_BOOT) || (ram && ~i_RW && ~i_BOOT);
-	assign o_EVENRAM_n = ~(isram && ~i_UDS_n);
-	assign o_ODDRAM_n = ~(isram && ~i_LDS_n);
+	wire ram = (!i_CPUSP_n && !i_AS_n && (i_A[23:20] == 4'h0));
+	wire isram = (ram && i_BOOT) || (ram && !i_A[19] && !i_BOOT) || (ram && !i_A[18] && !i_BOOT) || (ram && !i_RW && !i_BOOT);
+	assign o_EVENRAM_n = !(isram && !i_UDS_n);
+	assign o_ODDRAM_n = !(isram && !i_LDS_n);
 
-	wire rom = (~i_CPUSP_n && ~i_AS_n && (i_A[23:20] == 4'hE)) || (~i_CPUSP_n && ~i_AS_n && (i_A[23:18] == 6'b000000) && i_RW && ~i_BOOT);
-	assign o_EVENROM_n = ~(rom && ~i_UDS_n);
-	assign o_ODDROM_n = ~(rom && ~i_LDS_n);
+	wire rom = (!i_CPUSP_n && !i_AS_n && (i_A[23:20] == 4'hE)) || (!i_CPUSP_n && !i_AS_n && (i_A[23:18] == 6'b000000) && i_RW && !i_BOOT);
+	assign o_EVENROM_n = !(rom && !i_UDS_n);
+	assign o_ODDROM_n = !(rom && !i_LDS_n);
 	
-	assign o_IOSEL_n = ~(~i_CPUSP_n && (i_A[23:20] == 4'hF));
-	assign o_EXPSEL_n = ~(~i_CPUSP_n && (i_A[23:20] >= 4'h1) && (i_A[23:20] <= 4'hD));
+	assign o_IOSEL_n = !(!i_CPUSP_n && (i_A[23:20] == 4'hF));
+	assign o_EXPSEL_n = !(!i_CPUSP_n && (i_A[23:20] >= 4'h1) && (i_A[23:20] <= 4'hD));
 	
-	assign o_WR = ~i_RW;
+	assign o_WR = !i_RW;
 	
-	wire PPDTACK = (~i_CPUSP_n && (~o_EVENROM_n || ~o_ODDROM_n || ~o_EVENRAM_n|| ~o_ODDRAM_n || (~i_LGEXP_n && ~o_EXPSEL_n)));
-	assign o_DTACK_n = PPDTACK ? 1'b0 : 1'bZ;
+	// wire PPDTACK = (!i_CPUSP_n && (!o_EVENROM_n || !o_ODDROM_n || !o_EVENRAM_n|| !o_ODDRAM_n || (!i_LGEXP_n && !o_EXPSEL_n)));
+	// assign o_DTACK_n = PPDTACK ? 1'b0 : 1'bZ;
+
+	assign o_DTACK_n (!i_CPUSP_n && (!o_EVENROM_n || !o_ODDROM_n || !o_EVENRAM_n|| !o_ODDRAM_n || (!i_LGEXP_n && !o_EXPSEL_n)))  ? 1'b0 : 1'bZ;
+
 endmodule
--- Glue.v
+++ Glue.v
@@ -21,23 +21,22 @@ module Glue(
 	
 	assign o_HALT_n = i_HWRST ? 1'b0 : 1'bZ;
 	assign o_RESET_n = i_HWRST ? 1'b0 : 1'bZ;
-	assign o_RUNLED = o_HALT_n;
+	assign o_RUNLED =  i_HWRST ? 1'b0 : 1'b1; // !i_HWRST didnt seem to work becuase of the possible Z value ?
 
-	assign o_CPUSP_n = ~i_HWRST && (i_FC[2:0] == 3'b111);
-	assign o_DUIACK_n = ~(~i_HWRST && (i_FC[2:0] == 3'b111) && ~i_AS_n && (i_A[19] == 1) && (i_A_LOW[3:1] == 3'b100));
+	assign o_CPUSP_n = !(!i_HWRST && (i_FC[2:0] == 3'b111));
+	assign o_DUIACK_n = !(!o_CPUSP_n && !i_AS_n && (i_A[19] == 1) && (i_A_LOW[3:1] == 3'b100));
 
-    // the GALasm for setting the boot signal in the original code is using a trick that 
-    // I don't understand! I will just use a counter to set the boot signal after 4 cycles of AS
-    // which is the more typical approach (I think)
+	// The GALasm uses A19/A18 to spot whne the first 4 reads have completed
+	// Here we will count AS (memory access) cycles to set BOOT for the first 4 memory reads
 
     reg [2:0] counter = 3'b0;
 
     always @(posedge i_AS_n) begin
-	    if (~o_RESET_n && ~o_HALT_n) begin 
+	    if (!o_RESET_n && !o_HALT_n) begin 
 		    counter <= 0;
 		    o_BOOT <= 1'b0;
 	    end	else begin
-		    if (~o_BOOT) begin
+		    if (!o_BOOT) begin
 			    counter <= counter + 3'b001;
 			    if (counter == 3'b100) 
                     o_BOOT <= 1'b1;
@@ -45,3 +44,21 @@ module Glue(
 	    end
     end
 endmodule
+
+// Pin assignments for yosys flow
+//PIN: CHIP "Glue" ASSIGNED TO AN PLCC44
+//PIN: i_A        : 12
+//PIN: o_RESET_n  : 11
+//PIN: o_BOOT     : 9
+//PIN: o_HALT_n   : 8
+//PIN: o_RUNLED   : 6
+//PIN: o_CPUSP_n  : 5
+//PIN: o_DUIACK_n : 4
+//PIN: i_A_LOW_2  : 21
+//PIN: i_A_LOW_1  : 20
+//PIN: i_A_LOW_0  : 19
+//PIN: i_FC_2     : 18
+//PIN: i_FC_1     : 17
+//PIN: i_FC_0     : 16
+//PIN: i_AS_n     : 43
+//PIN: i_HWRST    : 44
